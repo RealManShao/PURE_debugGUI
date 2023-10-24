59ecb7dd8b79ad7490ae5bee2390dd4c0785bfe9
Ivan <xihe.21@intl.zju.edu.cn>
2023-10-24 11:13:03 -0500

    fix bugs

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ball_wheel_GUI
+## Intro
+This is a GUI used for PURE, based on PyQt5
+## Progress
+3, Oct: Create a UI demo by Qt Designer
+
+10, Oct: Reading the real-time plot code
+
+17, Oct: Reading Ti board communication code (by Yixiao)
+
+24, Oct: Give a demo of real time plot
--- Real-timePlot.py
+++ Real-timePlot.py
@@ -7,22 +7,25 @@ app = pg.mkQApp("Plotting Example")
 #mw = QtWidgets.QMainWindow()
 #mw.resize(800,800)
 
-win = pg.GraphicsLayoutWidget(show=True, title="Basic plotting examples")
+win = pg.GraphicsLayoutWidget(show=True, title="plotting examples")
 win.resize(1000,600)
-win.setWindowTitle('pyqtgraph example: Plotting')
+win.setWindowTitle('Plotting')
 
-plot = win.addPlot(title="Updating plot")
-curve = plot.plot(pen='y')
-data = np.sin(np.linspace(-np.pi/2,7/2*np.pi,200))
+plot1 = win.addPlot(title="Updating plot")
+plot1.setYRange(-2,2)
+curve = plot1.plot(pen='y')
+data = np.sin(np.linspace(-1/2*np.pi,3/2*np.pi,100))
 
 ptr = 0
+i=[0]*100 #create a empty list to hold the plot data
 def update():
-    global curve, data, ptr, plot
-    c=99-ptr%100
-    i=data[c:c+101:1]
+    global curve, data, ptr, plot1,i
+    c=ptr%100
+    i.pop(0)
+    i.append(data[c])
     curve.setData(i)
-    if ptr == 0:
-        plot.enableAutoRange('xy', False)  ## stop auto-scaling after the first data set is plotted
+    # if ptr == 0:
+    #     plot.enableAutoRange('xy', False)  ## stop auto-scaling after the first data set is plotted
     ptr += 1
 timer = QtCore.QTimer()
 timer.timeout.connect(update)
